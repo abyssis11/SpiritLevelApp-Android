1f9f00a68d072af5de9ef35e9b7996586c9f184d
Deni <denikernjus@gmail.com>
2023-01-21 14:27:56 +0100

    added shake detector and inclanation

--- app/home/home-page.js
+++ app/home/home-page.js
@@ -1,29 +1,80 @@
-import { startAccelerometerUpdates } from "nativescript-accelerometer";
+import { View } from '@nativescript/core';
+import { startAccelerometerUpdates, stopAccelerometerUpdates, isListening } from "nativescript-accelerometer";
 import { Observable } from '@nativescript/core';
+import { Shake } from './home-view-model';
 
 const context = new Observable();
-
+let shakeView = new View;
+const shakeDetector = new Shake(() => {
+    // ovo ce se izvrsiti pri pozivu onSensorData() funkcije (ako su zadovoljeni uvjeti)
+    shakeView.opacity = 1;
+    shakeView.scaleX = 2;
+    shakeView.scaleY = 2;
+    shakeView.animate({
+        duration: 1000,
+        opacity: 0,
+        curve: "easeOut"
+    });
+});
 
 function CalculateInclanation(data) {
-    let inclanation = Math.atan2(data.y, data.x) * (180/Math.PI);
-    if (inclanation < 0){
-        inclanation = 360 + inclanation;
+    function deg(inc){
+        if(inc < 0){
+            inc = 360+inc;
+        }
+        return inc
+    }
+    let inclanation = {
+        roll: Math.abs(Math.atan2(data.y, data.z) * (180/Math.PI) - 180),
+        pitch: deg(Math.atan2(-data.x, Math.sqrt(Math.pow(data.y, 2) + Math.pow(data.z, 2))) * (180/Math.PI))
     }
     return inclanation
 }
 
-// funkcija koja azurira vrijednosti x, y, z i prikazujemo da je detektiran shake
+// funkcija koja azurira vrijednosti x, y i prikazujemo da je detektiran shake
 function update(data) {
     let ykoordinata = (data.y.toFixed(2) * 50 + 50) + "*," + (100 - (data.y.toFixed(2) * 50 + 50)) + "*";
     let xkoordinata = (data.x.toFixed(2) * 50 + 50) + "*," + (100 - (data.x.toFixed(2) * 50 + 50)) + "*";
-    
+
     context.set("x", xkoordinata);
     context.set("y", ykoordinata);
     context.set("z", data.z.toFixed(2));
-    context.set("inclanation", CalculateInclanation(data));
+    context.set("roll", CalculateInclanation(data).roll.toFixed(2));
+    context.set("pitch", CalculateInclanation(data).pitch.toFixed(2));
+
+    shakeDetector.onSensorData(data);
 }
 
+// Event handler za "navigatingTo" event u home-page.xml
+export function navigatingTo(args) {
+    let page = args.object;
+    page.bindingContext = context;
+    shakeView = page.getViewById("shake-view");
+    context.set("button", "button-zaustavi");
+    try {
+        startAccelerometerUpdates(update, { sensorDelay: "ui" });
+    } catch (e) {
+        alert("Error: " + e.message);
+    }
 
-startAccelerometerUpdates(update, { sensorDelay: "ui" });
+    context.set("isListening", isListening());
+}
+
+// funkcija za gumb
+export function toggleUpdates() {
+    if (isListening()) {
+        stopAccelerometerUpdates();
+        context.set("button", "button-pokreni");
+    } else {
+        try {
+            startAccelerometerUpdates(update, { sensorDelay: "ui" });
+            context.set("button", "button-zaustavi");
+        } catch (e) {
+            alert("Error: " + e.message);
+        }
+    }
+
+    context.set("isListening", isListening());
+}
 
 
--- app/home/home-view-model.js
+++ app/home/home-view-model.js
@@ -1,9 +1,58 @@
-import { fromObject } from '@nativescript/core'
+import { time } from "@nativescript/core/profiling";
 
-export function HomeViewModel() {
-  const viewModel = fromObject({
-    /* Add your view model properties here */
-  })
+// koliki mora biti vektor sile da se smatra da je shake
+const FORCE_THRESHOLD = 0.2;
 
-  return viewModel
+// timeout izmedju dvije privjere shake-a u ms
+const TIME_THRESHOLD = 100;
+
+// timeout izmedju izvrsavanja cb funkcije u ms
+const SHAKE_TIMEOUT = 800;
+
+//razmak izmedju dva shakea u ms
+const SHAKE_THROTTLE = 1000;
+
+// nakon ovoliko shake-a ce se izvrsiti funkcija cb
+const SHAKE_COUNT = 2;
+
+export class Shake {
+    #lastTime = 0;
+    #lastShake = 0;
+    #lastForce = 0;
+    #shakeCount = 0;
+    #cb;
+
+    constructor(callback) {
+      // prosljedjenu funkciju spremamo u cb
+      this.#cb = zonedCallback(callback);
+    }
+
+
+    onSensorData(data) {
+      const now = time();
+      if ((now - this.#lastForce) > SHAKE_TIMEOUT) {
+          this.#shakeCount = 0;
+      }
+
+      const timeDelta = now - this.#lastTime;
+      if (timeDelta > TIME_THRESHOLD) {
+          // izracun za vektor sile
+          const forceVector = Math.abs(Math.sqrt(Math.pow(data.x, 2) + Math.pow(data.y, 2) + Math.pow(data.z, 2)) - 1);
+          // ako je vektor dovoljno "velik" onda se smatra ko shake
+          if (forceVector > FORCE_THRESHOLD) {
+              this.#shakeCount++;
+              // ako su detektirana 2 (SHAKE_COUNT) shaka i ako je proslo 1000 ms od proslog onda izvrsavamo cb
+              if ((this.#shakeCount >= SHAKE_COUNT) && (now - this.#lastShake > SHAKE_THROTTLE)) {
+                  this.#lastShake = now;
+                  this.#shakeCount = 0;
+                  
+                  // izvrsavamo cb, to ce biti animacija teksta "Umirite se!"
+                  this.#cb();
+              }
+              this.#lastForce = now;
+          }
+
+          this.#lastTime = now;
+      }
+  }
 }
